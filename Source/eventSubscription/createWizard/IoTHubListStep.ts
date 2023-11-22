cd8ad73364a0a6b08a3dba625ccab9b5c61e2630
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-22 10:35:35 +0200

    squash!

--- Source/eventSubscription/createWizard/IoTHubListStep.ts
+++ Source/eventSubscription/createWizard/IoTHubListStep.ts
@@ -3,37 +3,59 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { IotHubClient } from 'azure-arm-iothub';
-import { IotHubDescription } from 'azure-arm-iothub/lib/models';
-import { AzureWizardPromptStep, createAzureClient, IAzureQuickPickItem, IAzureQuickPickOptions, ISubscriptionWizardContext } from 'vscode-azureextensionui';
-import { ext } from '../../extensionVariables';
-import { localize } from '../../utils/localize';
+import { IotHubClient } from "azure-arm-iothub";
+import { IotHubDescription } from "azure-arm-iothub/lib/models";
+import {
+	AzureWizardPromptStep,
+	createAzureClient,
+	IAzureQuickPickItem,
+	IAzureQuickPickOptions,
+	ISubscriptionWizardContext,
+} from "vscode-azureextensionui";
+import { ext } from "../../extensionVariables";
+import { localize } from "../../utils/localize";
 
 export interface IIoTHubWizardContext extends ISubscriptionWizardContext {
-    iotHub?: IotHubDescription;
+	iotHub?: IotHubDescription;
 }
 
-export class IoTHubListStep<T extends IIoTHubWizardContext> extends AzureWizardPromptStep<T> {
-    public async prompt(wizardContext: T): Promise<T> {
-        if (!wizardContext.iotHub) {
-            const quickPickOptions: IAzureQuickPickOptions = { placeHolder: localize('listPlaceHolder', 'Select an IoT Hub'), id: `IoTHubListStep/${wizardContext.subscriptionId}` };
-            wizardContext.iotHub = (await ext.ui.showQuickPick(this.getQuickPicks(wizardContext), quickPickOptions)).data;
-        }
+export class IoTHubListStep<
+	T extends IIoTHubWizardContext,
+> extends AzureWizardPromptStep<T> {
+	public async prompt(wizardContext: T): Promise<T> {
+		if (!wizardContext.iotHub) {
+			const quickPickOptions: IAzureQuickPickOptions = {
+				placeHolder: localize("listPlaceHolder", "Select an IoT Hub"),
+				id: `IoTHubListStep/${wizardContext.subscriptionId}`,
+			};
+			wizardContext.iotHub = (
+				await ext.ui.showQuickPick(
+					this.getQuickPicks(wizardContext),
+					quickPickOptions
+				)
+			).data;
+		}
 
-        return wizardContext;
-    }
+		return wizardContext;
+	}
 
-    private async getQuickPicks(wizardContext: T): Promise<IAzureQuickPickItem<IotHubDescription>[]> {
-        const client: IotHubClient = createAzureClient(wizardContext, IotHubClient);
-        const hubs: IotHubDescription[] = await client.iotHubResource.listBySubscription();
-        return hubs.map((h: IotHubDescription) => {
-            return {
-                id: h.id,
-                // tslint:disable-next-line:no-non-null-assertion
-                label: h.name!,
-                description: '',
-                data: h
-            };
-        });
-    }
+	private async getQuickPicks(
+		wizardContext: T
+	): Promise<IAzureQuickPickItem<IotHubDescription>[]> {
+		const client: IotHubClient = createAzureClient(
+			wizardContext,
+			IotHubClient
+		);
+		const hubs: IotHubDescription[] =
+			await client.iotHubResource.listBySubscription();
+		return hubs.map((h: IotHubDescription) => {
+			return {
+				id: h.id,
+				// tslint:disable-next-line:no-non-null-assertion
+				label: h.name!,
+				description: "",
+				data: h,
+			};
+		});
+	}
 }
--- Source/eventSubscription/tree/EventSubscriptionProvider.ts
+++ Source/eventSubscription/tree/EventSubscriptionProvider.ts
@@ -3,81 +3,133 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { EventGridManagementClient } from 'azure-arm-eventgrid';
-import { EventSubscription } from 'azure-arm-eventgrid/lib/models';
-import { SubscriptionClient } from 'azure-arm-resource';
-import { Location } from 'azure-arm-resource/lib/subscription/models';
-import * as vscode from 'vscode';
-import { AzureWizard, createAzureClient, createAzureSubscriptionClient, IActionContext, ISubscriptionRoot, parseError, SubscriptionTreeItem } from 'vscode-azureextensionui';
-import { localize } from '../../utils/localize';
-import { EndpointUrlStep } from '../createWizard/EndpointUrlStep';
-import { EventSubscriptionCreateStep } from '../createWizard/EventSubscriptionCreateStep';
-import { EventSubscriptionNameStep } from '../createWizard/EventSubscriptionNameStep';
-import { IEventSubscriptionWizardContext } from '../createWizard/IEventSubscriptionWizardContext';
-import { TopicTypeStep } from '../createWizard/TopicTypeStep';
-import { EventSubscriptionTreeItem } from './EventSubscriptionTreeItem';
+import { EventGridManagementClient } from "azure-arm-eventgrid";
+import { EventSubscription } from "azure-arm-eventgrid/lib/models";
+import { SubscriptionClient } from "azure-arm-resource";
+import { Location } from "azure-arm-resource/lib/subscription/models";
+import * as vscode from "vscode";
+import {
+	AzureWizard,
+	createAzureClient,
+	createAzureSubscriptionClient,
+	IActionContext,
+	ISubscriptionRoot,
+	parseError,
+	SubscriptionTreeItem,
+} from "vscode-azureextensionui";
+import { localize } from "../../utils/localize";
+import { EndpointUrlStep } from "../createWizard/EndpointUrlStep";
+import { EventSubscriptionCreateStep } from "../createWizard/EventSubscriptionCreateStep";
+import { EventSubscriptionNameStep } from "../createWizard/EventSubscriptionNameStep";
+import { IEventSubscriptionWizardContext } from "../createWizard/IEventSubscriptionWizardContext";
+import { TopicTypeStep } from "../createWizard/TopicTypeStep";
+import { EventSubscriptionTreeItem } from "./EventSubscriptionTreeItem";
 
 export class EventSubscriptionProvider extends SubscriptionTreeItem {
-    public readonly childTypeLabel: string = localize('eventSubscription', 'Event Subscription');
+	public readonly childTypeLabel: string = localize(
+		"eventSubscription",
+		"Event Subscription"
+	);
 
-    public hasMoreChildrenImpl(): boolean {
-        return false;
-    }
+	public hasMoreChildrenImpl(): boolean {
+		return false;
+	}
 
-    public async loadMoreChildrenImpl(_clearCache: boolean): Promise<EventSubscriptionTreeItem[]> {
-        const client: EventGridManagementClient = createAzureClient(this.root, EventGridManagementClient);
+	public async loadMoreChildrenImpl(
+		_clearCache: boolean
+	): Promise<EventSubscriptionTreeItem[]> {
+		const client: EventGridManagementClient = createAzureClient(
+			this.root,
+			EventGridManagementClient
+		);
 
-        // There is no "listAll" method - we have to list individually by location
-        const listByLocationTasks: Promise<EventSubscription[]>[] = (await listLocations(this.root)).map(async (location: Location) => {
-            try {
-                // tslint:disable-next-line:no-non-null-assertion
-                return await client.eventSubscriptions.listRegionalBySubscription(location.name!);
-            } catch (error) {
-                // Ignore errors for regions where EventGrid is not supported
-                if (parseError(error).errorType === 'NoRegisteredProviderFound') {
-                    return [];
-                } else {
-                    throw error;
-                }
-            }
-        });
-        const eventSubscriptions: EventSubscription[] = (<EventSubscription[]>[]).concat(...(await Promise.all([client.eventSubscriptions.listGlobalBySubscription()].concat(...listByLocationTasks))));
-        return eventSubscriptions.map((es: EventSubscription) => new EventSubscriptionTreeItem(this, es));
-    }
+		// There is no "listAll" method - we have to list individually by location
+		const listByLocationTasks: Promise<EventSubscription[]>[] = (
+			await listLocations(this.root)
+		).map(async (location: Location) => {
+			try {
+				// tslint:disable-next-line:no-non-null-assertion
+				return await client.eventSubscriptions.listRegionalBySubscription(
+					location.name!
+				);
+			} catch (error) {
+				// Ignore errors for regions where EventGrid is not supported
+				if (
+					parseError(error).errorType === "NoRegisteredProviderFound"
+				) {
+					return [];
+				} else {
+					throw error;
+				}
+			}
+		});
+		const eventSubscriptions: EventSubscription[] = (<
+			EventSubscription[]
+		>[]).concat(
+			...(await Promise.all(
+				[client.eventSubscriptions.listGlobalBySubscription()].concat(
+					...listByLocationTasks
+				)
+			))
+		);
+		return eventSubscriptions.map(
+			(es: EventSubscription) => new EventSubscriptionTreeItem(this, es)
+		);
+	}
 
-    public async createChildImpl(showCreatingNode: (label: string) => void, actionContext?: IActionContext): Promise<EventSubscriptionTreeItem> {
-        const wizardContext: IEventSubscriptionWizardContext = Object.assign({}, this.root);
+	public async createChildImpl(
+		showCreatingNode: (label: string) => void,
+		actionContext?: IActionContext
+	): Promise<EventSubscriptionTreeItem> {
+		const wizardContext: IEventSubscriptionWizardContext = Object.assign(
+			{},
+			this.root
+		);
 
-        const wizard: AzureWizard<IEventSubscriptionWizardContext> = new AzureWizard(
-            [
-                new EventSubscriptionNameStep(),
-                new TopicTypeStep(),
-                new EndpointUrlStep()
-            ],
-            [
-                new EventSubscriptionCreateStep()
-            ],
-            wizardContext
-        );
+		const wizard: AzureWizard<IEventSubscriptionWizardContext> =
+			new AzureWizard(
+				[
+					new EventSubscriptionNameStep(),
+					new TopicTypeStep(),
+					new EndpointUrlStep(),
+				],
+				[new EventSubscriptionCreateStep()],
+				wizardContext
+			);
 
-        // https://github.com/Microsoft/vscode-azuretools/issues/120
-        // tslint:disable-next-line:strict-boolean-expressions
-        actionContext = actionContext || <IActionContext>{ properties: {}, measurements: {} };
+		// https://github.com/Microsoft/vscode-azuretools/issues/120
+		// tslint:disable-next-line:strict-boolean-expressions
+		actionContext =
+			actionContext ||
+			<IActionContext>{ properties: {}, measurements: {} };
 
-        await wizard.prompt(actionContext);
-        // tslint:disable-next-line:no-non-null-assertion
-        showCreatingNode(wizardContext.newEventSubscriptionName!);
-        const message: string = localize('creatingEventSubscription', 'Creating event subscription "{0}"...', wizardContext.newEventSubscriptionName);
-        await vscode.window.withProgress({ title: message, location: vscode.ProgressLocation.Notification }, async () => {
-            // tslint:disable-next-line:no-non-null-assertion
-            await wizard.execute(actionContext!);
-        });
-        // tslint:disable-next-line:no-non-null-assertion
-        return new EventSubscriptionTreeItem(this, wizardContext.eventSubscription!);
-    }
+		await wizard.prompt(actionContext);
+		// tslint:disable-next-line:no-non-null-assertion
+		showCreatingNode(wizardContext.newEventSubscriptionName!);
+		const message: string = localize(
+			"creatingEventSubscription",
+			'Creating event subscription "{0}"...',
+			wizardContext.newEventSubscriptionName
+		);
+		await vscode.window.withProgress(
+			{ title: message, location: vscode.ProgressLocation.Notification },
+			async () => {
+				// tslint:disable-next-line:no-non-null-assertion
+				await wizard.execute(actionContext!);
+			}
+		);
+		// tslint:disable-next-line:no-non-null-assertion
+		return new EventSubscriptionTreeItem(
+			this,
+			wizardContext.eventSubscription!
+		);
+	}
 }
 
 async function listLocations(root: ISubscriptionRoot): Promise<Location[]> {
-    const client: SubscriptionClient = createAzureSubscriptionClient(root, SubscriptionClient);
-    return await client.subscriptions.listLocations(root.subscriptionId);
+	const client: SubscriptionClient = createAzureSubscriptionClient(
+		root,
+		SubscriptionClient
+	);
+	return await client.subscriptions.listLocations(root.subscriptionId);
 }
