cdee179f88127e224e17452592a3cefa111ea052
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-24 06:44:40 +0200

    squash!

--- Source/eventSubscription/commands/mock/previewEvents.ts
+++ Source/eventSubscription/commands/mock/previewEvents.ts
@@ -9,7 +9,16 @@ import { generateEvents } from "./generateEvents";
 import { IMockEventGenerator } from "./IMockEventGenerator";
 
 export async function previewEvents(uri: Uri): Promise<void> {
-    const [events]: [{}[], IMockEventGenerator] = await generateEvents(uri);
-    const viewColumn: number | undefined = window.activeTextEditor !== undefined && window.activeTextEditor.viewColumn !== undefined ? window.activeTextEditor.viewColumn + 1 : undefined;
-    await fsUtils.showNewFile(JSON.stringify(events, undefined, 2), 'eventsPreview', '.json', viewColumn);
+	const [events]: [{}[], IMockEventGenerator] = await generateEvents(uri);
+	const viewColumn: number | undefined =
+		window.activeTextEditor !== undefined &&
+		window.activeTextEditor.viewColumn !== undefined
+			? window.activeTextEditor.viewColumn + 1
+			: undefined;
+	await fsUtils.showNewFile(
+		JSON.stringify(events, undefined, 2),
+		"eventsPreview",
+		".json",
+		viewColumn
+	);
 }
